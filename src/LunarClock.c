1dd4c9baa7ae0c0127351799b0e6c585b7c143b5
William Heaton <William.G.Heaton@gmail.com>
2013-04-18 15:11:38 -0400

    Clean up code

--- src/LunarClock.c
+++ src/LunarClock.c
@@ -17,8 +17,12 @@ Window window;
 Layer moon_layer;
 Layer shadow_layer;
 
-bool leapYear(int year){
+int radius  = 70;
+int border = 1;
+int centerx = 71;
+int centery = 167-70;
 
+bool leapYear(int year){
     if(year%400==0)
         return true;
     else if(year%100==0)
@@ -28,8 +32,6 @@ bool leapYear(int year){
     else 
         return false;
 }
-
-
 int daysSinceJan_1_1900( int year, int yday){
      
     int delta = yday;
@@ -38,71 +40,45 @@ int daysSinceJan_1_1900( int year, int yday){
         if(leapYear(i))
             delta += 1;            
     }
-    
     return delta;
 }
 
 double phase(){
-
     PblTm currentTime;
     get_time(&currentTime);
     double delta = daysSinceJan_1_1900(currentTime.tm_year+1900,currentTime.tm_yday);
     double p = 29.530588853;
-    
     return delta - p * floor(delta / p);
 }
 
 double phasePercent(){
-
     return phase()/29.530588853;
 }
 
 char* intToStr(int val){
-
  	static char buf[32] = {0};
-	
 	int i = 30;	
 	for(; val && i ; --i, val /= 10)
 		buf[i] = "0123456789"[val % 10];
 	
 	return &buf[i+1];
 }
-int radius  = 70;
-int centerx = 71;
-int centery = 167-70;
+
 void moon_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
-    
-    graphics_context_set_stroke_color(ctx, GColorWhite);
     graphics_context_set_fill_color(ctx, GColorWhite);
-    graphics_context_set_text_color(ctx, GColorWhite);
-    
-    //phase();
-    
-    graphics_text_draw(ctx, intToStr(phase()*100),  fonts_get_system_font(FONT_KEY_GOTHIC_24), GRect(0, 0, 144, 30), GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
-    
     graphics_fill_circle(ctx, GPoint(centerx,centery), radius);
-
 }
+
 void shadow_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
-    
-    double p = phasePercent();
-//    graphics_text_draw(ctx, intToStr(p*100),  fonts_get_system_font(FONT_KEY_GOTHIC_24), GRect(0, 30, 144, 30), GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
-    
-    p = 2.0/8.0;
     
     graphics_context_set_stroke_color(ctx, GColorBlack);
     graphics_context_set_fill_color(ctx, GColorBlack);
     graphics_context_set_text_color(ctx, GColorBlack);
-
-    int x0 = centerx;
-    int y0 = centery;
-    int f = 1 - radius;
-    int ddF_x = 1;
-    int ddF_y = -2 * radius;
-    int x = 0;
-    int y = radius-1;
+    
+    double p = phasePercent();    
+    
     double off2;
     int off1 = -1;
     if(p < 0.5)
@@ -111,18 +87,16 @@ void shadow_layer_update_callback(Layer *me, GContext* ctx) {
         off2 = -(4.*p-3.);
         off1 = 1;
     }
- //   graphics_draw_pixel(ctx, GPoint(x0, y0 + radius));
- //   graphics_draw_pixel(ctx, GPoint(x0, y0 - radius));
- //   graphics_draw_pixel(ctx, GPoint(x0 + radius, y0));
- //   graphics_draw_pixel(ctx, GPoint(x0 - radius, y0));
- 
-    graphics_draw_line(ctx, GPoint(x0 + off1*radius, y0), GPoint((x0 + off2*radius), y0));
-        
-        
-    while(x < y)
-    {
-        if(f >= 0) 
-        {
+    
+    int r = radius - border;
+    int f = 1 - r;
+    int ddF_x = 1;
+    int ddF_y = -2 * r;
+    int x = 0;
+    int y = r;
+    
+    while(x < y){
+        if(f >= 0) {
             y--;
             ddF_y += 2;
             f += ddF_y;
@@ -131,13 +105,12 @@ void shadow_layer_update_callback(Layer *me, GContext* ctx) {
         ddF_x += 2;
         f += ddF_x;  
         
-        graphics_draw_line(ctx, GPoint(x0 + off1*x, y0 + y), GPoint(x0 + off2*x, y0 + y));
-        graphics_draw_line(ctx, GPoint(x0 + off1*x, y0 - y), GPoint(x0 + off2*x, y0 - y));
-        graphics_draw_line(ctx, GPoint(x0 + off1*y, y0 + x), GPoint(x0 + off2*y, y0 + x));
-        graphics_draw_line(ctx, GPoint(x0 + off1*y, y0 - x), GPoint(x0 + off2*y, y0 - x));
+        graphics_draw_line(ctx, GPoint(centerx + off1*x, centery + y), GPoint(centerx + off2*x, centery + y));
+        graphics_draw_line(ctx, GPoint(centerx + off1*x, centery - y), GPoint(centerx + off2*x, centery - y));
+        graphics_draw_line(ctx, GPoint(centerx + off1*y, centery + x), GPoint(centerx + off2*y, centery + x));
+        graphics_draw_line(ctx, GPoint(centerx + off1*y, centery - x), GPoint(centerx + off2*y, centery - x));
     }
-
-    
+    graphics_draw_line(ctx, GPoint(centerx + off1*r, centery), GPoint((centerx + off2*r), centery));
 }
 
 
@@ -158,10 +131,19 @@ void handle_init(AppContextRef ctx) {
     layer_add_child(&window.layer, &shadow_layer);
 }
 
+void day_tick(AppContextRef ctx, PebbleTickEvent *t) {
+    (void)ctx;
+    layer_mark_dirty(&shadow_layer);
+}
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
+    .init_handler = &handle_init,
+
+    .tick_info = {
+        .tick_handler = &day_tick,
+        .tick_units = DAY_UNIT
+    }
   };
   app_event_loop(params, &handlers);
 }
