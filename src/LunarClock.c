95294fb2544352ac0e332915d39cbb65e3d0473a
William Heaton <William.G.Heaton@gmail.com>
2013-04-20 13:10:38 -0400

    Clean Up Code

--- src/LunarClock.c
+++ src/LunarClock.c
@@ -14,14 +14,6 @@ PBL_APP_INFO(MY_UUID,
 
 Window window;
 
-Layer moon_layer;
-Layer shadow_layer;
-Layer phase_layer;
-Layer second_layer;
-Layer minute_layer;
-Layer hour_layer;
-
-
 bool showSeconds = true;
 int timezone = -5;
 
@@ -30,15 +22,26 @@ int border = 1;
 int centerx = 71;
 int centery = 71;
 
+double hourLen = 0.60;
+double minLen  = 0.75;
+double secLen  = 0.88;
+int    secRad  = 3;
 
-double secLen = 0.80;
-int    secRad = 3;
-double minLen = 0.80;
-double minAng = 4./360.;
-double minCP  = 0.7;
-double houLen = 0.60;
+const char phases[8][20] = {"New Moon","Waxing Crescent","First Quarter","Waxing Gibbous","Full Moon","Waning Gibbous","Third Quarter","Waning Crescent"};
 
-const char phases[8][20] = {"New Moon","Waxing Crescent","First Quarter","Waxing Gibbous","Full Moon","Waning Gibbous","Thrid Quarter","Waning Crescent"};
+int ph;
+double phase;
+double phasePercent;
+int curHour;
+int curMin;
+int curSec;
+
+Layer moon_layer;
+Layer shadow_layer;
+Layer phase_layer;
+Layer second_layer;
+Layer minute_layer;
+Layer hour_layer;
 
 
 bool leapYear(int year){
@@ -62,18 +65,6 @@ double daysSinceNewMoon( int year, int yday, int hour){
     return delta+(hour+5+timezone)/24.;
 }
 
-double phase(){
-    PblTm currentTime;
-    get_time(&currentTime);
-    double delta = daysSinceNewMoon(currentTime.tm_year+1900,currentTime.tm_yday,currentTime.tm_hour);
-    double p = 29.530588853;
-    return delta - p * floor(delta / p);
-}
-
-double phasePercent(){
-    return phase()/29.530588853;
-}
-
 char* intToStr(int val){
  	static char buf[32] = {0};
 	int i = 30;	
@@ -83,149 +74,29 @@ char* intToStr(int val){
 	return &buf[i+1];
 }
 
+const int cols[8][3] = 
+                    {{GColorWhite,GColorWhite,GColorBlack},
+                    {GColorBlack,GColorWhite},
+                    {GColorBlack,GColorWhite},
+                    {GColorBlack,GColorWhite},
+                    {GColorBlack,GColorBlack,GColorWhite},
+                    {GColorWhite,GColorBlack},
+                    {GColorWhite,GColorBlack},
+                    {GColorWhite,GColorBlack}};
 
-void hour_layer_update_callback(Layer *me, GContext* ctx) {
-    (void)me;
-    PblTm currentTime;
-    get_time(&currentTime);
-    int hour = currentTime.tm_hour%12;
-    int min = currentTime.tm_min;
-    int cols[8][3] = 
-                        {{GColorWhite,GColorWhite,GColorBlack},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorBlack,GColorWhite},
-                        {GColorWhite,GColorBlack},
-                        {GColorWhite,GColorBlack},
-                        {GColorWhite,GColorBlack}};
+void drawHand(Layer *me, GContext* ctx,double sec,int r,bool endCirc,bool startCirc){
     
-    int p = floor(16.*(phasePercent()+0.09));
-    p = (p % 16)/2;
-        
+    int c1 = cols[ph][sec>30?1:0];
+    int c2 = cols[ph][sec>30?0:1];
+    int c3 = (c1==c2)?cols[ph][2]:c2;
+    
     int ox = -1;
     int oy = 1;
-    int c1 = cols[p][0];
-    int c2 = cols[p][1];
-    if(hour<3 || hour>9){
-        oy = -1;
-    }if(hour>6){
+    if(sec>30){
         ox = 1;
-        c1 = cols[p][1];
-        c2 = cols[p][0];
-    }
-    
-    int c3 = (c1==c2)?cols[p][2]:c2;
-    
-    double angle = TRIG_MAX_ANGLE/-4.+TRIG_MAX_ANGLE * (min/60.+hour)/12.;
-    double c = cos_lookup(angle) / 65536.;
-    double s = sin_lookup(angle) / 65536.;
-    
-    graphics_context_set_stroke_color(ctx, c3);
-    graphics_draw_line(
-        ctx, 
-        GPoint(centerx+ox, centery+oy), 
-        GPoint(
-            centerx + (c*radius*houLen)+ox, 
-            centery + (s*radius*houLen)+oy));
-    
-    graphics_context_set_stroke_color(ctx, c1);
-    graphics_draw_line(
-        ctx, 
-        GPoint(centerx, centery), 
-        GPoint(
-            centerx + (c*radius*houLen), 
-            centery + (s*radius*houLen)));
-    
-    graphics_context_set_fill_color(ctx, cols[p][0]);
-    graphics_fill_circle(ctx, GPoint(centerx,centery), 3);
-}
-
-void minute_layer_update_callback(Layer *me, GContext* ctx) {
-    (void)me;
-    PblTm currentTime;
-    get_time(&currentTime);
-    int min = currentTime.tm_min;
-    int cols[8][3] = 
-                        {{GColorWhite,GColorWhite,GColorBlack},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorBlack,GColorWhite},
-                        {GColorWhite,GColorBlack},
-                        {GColorWhite,GColorBlack},
-                        {GColorWhite,GColorBlack}};
-    
-    int p = floor(16.*(phasePercent()+0.09));
-    p = (p % 16)/2;
-        
-    int ox = -1;
-    int oy = 1;
-    int c1 = cols[p][0];
-    int c2 = cols[p][1];
-    if(min<15 || min>45){
+    }if(sec<15 || sec>45){
         oy = -1;
-    }if(min>30){
-        ox = 1;
-        c1 = cols[p][1];
-        c2 = cols[p][0];
     }
-    
-    int c3 = (c1==c2)?cols[p][2]:c2;
-    
-    double angle = TRIG_MAX_ANGLE/-4.+TRIG_MAX_ANGLE * min/60.;
-    double c = cos_lookup(angle) / 65536.;
-    double s = sin_lookup(angle) / 65536.;
-    
-    graphics_context_set_stroke_color(ctx, c3);
-    graphics_draw_line(
-        ctx, 
-        GPoint(centerx+ox, centery+oy), 
-        GPoint(
-            centerx + (c*radius*minLen)+ox, 
-            centery + (s*radius*minLen)+oy));
-    
-    graphics_context_set_stroke_color(ctx, c1);
-    graphics_draw_line(
-        ctx, 
-        GPoint(centerx, centery), 
-        GPoint(
-            centerx + (c*radius*minLen), 
-            centery + (s*radius*minLen)));
-    
-}
-
-void second_layer_update_callback(Layer *me, GContext* ctx) {
-    (void)me;
-    PblTm currentTime;
-    get_time(&currentTime);
-    int sec = currentTime.tm_sec;
-    int cols[8][3] = 
-                        {{GColorWhite,GColorWhite,GColorBlack},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorWhite},
-                        {GColorBlack,GColorBlack,GColorWhite},
-                        {GColorWhite,GColorBlack},
-                        {GColorWhite,GColorBlack},
-                        {GColorWhite,GColorBlack}};
-    
-    int p = floor(16.*(phasePercent()+0.09));
-    p = (p % 16)/2;
-        
-    int ox = -1;
-    int oy = 1;
-    int c1 = cols[p][0];
-    int c2 = cols[p][1];
-    if(sec<15 || sec>45){
-        oy = -1;
-    }if(sec>30){
-        ox = 1;
-        c1 = cols[p][1];
-        c2 = cols[p][0];
-    }
-    
-    int c3 = (c1==c2)?cols[p][2]:c2;
     
     double angle = TRIG_MAX_ANGLE/-4.+TRIG_MAX_ANGLE * sec/60.;
     double c = cos_lookup(angle) / 65536.;
@@ -236,25 +107,44 @@ void second_layer_update_callback(Layer *me, GContext* ctx) {
         ctx, 
         GPoint(centerx+ox, centery+oy), 
         GPoint(
-            centerx + (c*(radius*secLen+2*secRad))+ox, 
-            centery + (s*(radius*secLen+2*secRad))+oy));
+            centerx + c*r+ox, 
+            centery + s*r+oy));
     
     graphics_context_set_stroke_color(ctx, c1);
     graphics_draw_line(
         ctx, 
         GPoint(centerx, centery), 
         GPoint(
-            centerx + (c*(radius*secLen+2*secRad)), 
-            centery + (s*(radius*secLen+2*secRad))));
+            centerx + c*r, 
+            centery + s*r));
     
-    double cc = (radius + 2*secRad)*secLen;
-    graphics_context_set_fill_color(ctx, c1);
-    graphics_fill_circle(ctx, GPoint(centerx + c*cc,centery + s*cc), secRad);
-    graphics_context_set_fill_color(ctx, c3);
-    graphics_fill_circle(ctx, GPoint(centerx + c*cc,centery + s*cc), secRad-1);
-    
+    if(startCirc){
+        graphics_context_set_fill_color(ctx, c3);
+        graphics_fill_circle(ctx, GPoint(centerx,centery), 5);
+        graphics_context_set_fill_color(ctx, c1);
+        graphics_fill_circle(ctx, GPoint(centerx,centery), 2);
+    }
+    if(endCirc){
+        double cc = r+secRad;
+        graphics_context_set_fill_color(ctx, c1);
+        graphics_fill_circle(ctx, GPoint(centerx + c*cc,centery + s*cc), secRad);
+        graphics_context_set_fill_color(ctx, c3);
+        graphics_fill_circle(ctx, GPoint(centerx + c*cc,centery + s*cc), secRad-1);
+    }
+}
+void second_layer_update_callback(Layer *me, GContext* ctx) {
+    (void)me;
+    drawHand(me, ctx, curSec, radius*secLen,true,true);
 }
 
+void minute_layer_update_callback(Layer *me, GContext* ctx) {
+    (void)me;
+    drawHand(me, ctx, curMin, radius*minLen,false,false);
+}
+void hour_layer_update_callback(Layer *me, GContext* ctx) {
+    (void)me;
+    drawHand(me, ctx, ((curHour%12)+curMin/60.)*60./12., radius*hourLen,false,false);
+}
 void moon_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
     graphics_context_set_fill_color(ctx, GColorWhite);
@@ -263,19 +153,15 @@ void moon_layer_update_callback(Layer *me, GContext* ctx) {
 void phase_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
     
-    int p = floor(16.*(phasePercent()+0.0625));
-    p = (p % 16)/2;
-        
     graphics_context_set_text_color(ctx, GColorWhite);
     graphics_text_draw(
         ctx, 
-        phases[p],  
+        phases[ph],  
         fonts_get_system_font(FONT_KEY_GOTHIC_24), 
         GRect(0, 138, 144, 30), 
         GTextOverflowModeWordWrap, 
         GTextAlignmentCenter, 
         NULL);
-    
 }
 void shadow_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
@@ -284,7 +170,7 @@ void shadow_layer_update_callback(Layer *me, GContext* ctx) {
     graphics_context_set_fill_color(ctx, GColorBlack);
     graphics_context_set_text_color(ctx, GColorBlack);
     
-    double p = phasePercent();    
+    double p = phasePercent;
     
     double off2;
     int off1 = -1;
@@ -329,6 +215,17 @@ void handle_init(AppContextRef ctx) {
     window_stack_push(&window, true /* Animated */);
     window_set_background_color(&window, GColorBlack);
     
+    PblTm t;
+    get_time(&t);
+    double delta = daysSinceNewMoon(t.tm_year+1900,t.tm_yday,t.tm_hour);
+    phase = delta - 29.530588853 * floor(delta / 29.530588853);
+    phasePercent = phase/29.530588853;
+    ph = ((int)floor(16.*(phasePercent+0.09)) % 16)/2;
+    
+    curHour=t.tm_hour;
+    curMin=t.tm_min;
+    curSec=t.tm_sec;
+    
     layer_init(&moon_layer, window.layer.frame);
     moon_layer.update_proc = &moon_layer_update_callback;
     layer_add_child(&window.layer, &moon_layer);
@@ -341,29 +238,30 @@ void handle_init(AppContextRef ctx) {
     phase_layer.update_proc = &phase_layer_update_callback;
     layer_add_child(&window.layer, &phase_layer);
     
-    if(showSeconds){
+    layer_init(&hour_layer, window.layer.frame);
+    hour_layer.update_proc = &hour_layer_update_callback;
+    layer_add_child(&window.layer, &hour_layer);
     
-        layer_init(&second_layer, window.layer.frame);
-        second_layer.update_proc = &second_layer_update_callback;
-        layer_add_child(&window.layer, &second_layer);
-    
-    }
     layer_init(&minute_layer, window.layer.frame);
     minute_layer.update_proc = &minute_layer_update_callback;
     layer_add_child(&window.layer, &minute_layer);
     
-    layer_init(&hour_layer, window.layer.frame);
-    hour_layer.update_proc = &hour_layer_update_callback;
-    layer_add_child(&window.layer, &hour_layer);
+    if(showSeconds){
+        layer_init(&second_layer, window.layer.frame);
+        second_layer.update_proc = &second_layer_update_callback;
+        layer_add_child(&window.layer, &second_layer);
+    }
+    
+    
 }
 
 void second_tick(AppContextRef ctx, PebbleTickEvent *t) {
     (void)ctx;
     
-    if (t->units_changed & HOUR_UNIT) {
-        layer_mark_dirty(&shadow_layer);
-        layer_mark_dirty(&phase_layer);
-    }
+    curHour=t->tick_time->tm_hour;
+    curMin=t->tick_time->tm_min;
+    curSec=t->tick_time->tm_sec;
+
     if (showSeconds && (t->units_changed & SECOND_UNIT)) {
         layer_mark_dirty(&second_layer);
     }
@@ -371,8 +269,16 @@ void second_tick(AppContextRef ctx, PebbleTickEvent *t) {
         layer_mark_dirty(&minute_layer);
         layer_mark_dirty(&hour_layer);
     }
-
+    if (t->units_changed & HOUR_UNIT) {
     
+        double delta = daysSinceNewMoon(t->tick_time->tm_year+1900,t->tick_time->tm_yday,t->tick_time->tm_hour);
+        phase = delta - 29.530588853 * floor(delta / 29.530588853);
+        phasePercent = phase/29.530588853;
+        ph = ((int)floor(16.*(phasePercent+0.09)) % 16)/2;
+        
+        layer_mark_dirty(&shadow_layer);
+        layer_mark_dirty(&phase_layer);
+    }
 }
 
 void pbl_main(void *params) {
